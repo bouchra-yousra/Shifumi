dc0a5bb95142f4b1b925ab85021f5fc3716b0a20
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-12 15:11:44 +0100

    refactor choice list

---
+++ src/components/choices/Choices.tsx
@@ -0,0 +1,34 @@
+import { Button } from "..";
+import { GameChoice } from "../../types";
+import { ChoiceContainer } from "./ChoiceContainer";
+
+type ChoicesProps = {
+  playerChoice: GameChoice;
+  disabled?: boolean;
+  handleSelectChoice: (choice: GameChoice) => void;
+};
+
+export const Choices = ({
+  handleSelectChoice,
+  playerChoice,
+  disabled,
+}: ChoicesProps) => {
+  const choices: GameChoice[] = [
+    GameChoice.ROCK,
+    GameChoice.PAPER,
+    GameChoice.SCISSORS,
+  ];
+  return (
+    <ChoiceContainer>
+      {choices.map((choice) => (
+        <Button
+          onClick={() => handleSelectChoice(choice)}
+          selected={choice === playerChoice}
+          disabled={disabled}
+        >
+          {choice}
+        </Button>
+      ))}
+    </ChoiceContainer>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,16 @@ import { Button } from "./buttons/Button";
 import { GameResult } from "./gameResult/GameResult";
 import { Borad } from "./board/Borad";
 import { PlayGround } from "./playground/PlayGround";
+import { Container } from "./container/Container";
+import { ControlContainer } from "./container/ControlContainer";
+import { Choices } from "./choices/Choices";
 
-export { Button, GameResult, Borad, PlayGround };
+export {
+  Button,
+  GameResult,
+  Borad,
+  PlayGround,
+  Container,
+  ControlContainer,
+  Choices,
+};
