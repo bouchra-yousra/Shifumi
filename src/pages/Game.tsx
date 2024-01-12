25348901bd7f0ff94fdfc03e193e380ec2a8c74e
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-12 12:33:11 +0100

    update game: add board component & score system

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -3,8 +3,10 @@ import { GameChoice } from "../types/GameChoice";
 import { getRandomChoice } from "../services/getRandomChoice";
 import { getGameResult } from "../services/getGameResult";
 import { Result } from "../types/Results";
-import { Button } from "../components";
+import { Borad, Button } from "../components";
 import { GameResult } from "../components/gameResult/GameResult";
+import { Score } from "../types/Score";
+import { updateScore } from "../services/updateScore";
 
 export const Game = () => {
   const choices: GameChoice[] = [
@@ -20,19 +22,31 @@ export const Game = () => {
   const [playerChoice, setPlayerChoice] = useState<GameChoice>();
   const [computerChoice, setComputerChoice] = useState<GameChoice>();
   const [result, setResult] = useState<Result>();
+  const [score, setScore] = useState<Score>({
+    playerScore: 0,
+    computerScore: 0,
+  });
 
   const handleClick = (e: GameChoice) => {
     let randomChoice = getRandomChoice(choices);
     let gameResult = getGameResult(e, randomChoice);
+    let newScore = updateScore(gameResult, score);
 
     setCurrentRound((val: number) => val + 1);
     setPlayerChoice(e);
     setComputerChoice(randomChoice);
     setResult(gameResult);
+    setScore(newScore);
   };
 
   return (
     <div>
+      <Borad
+        playerScore={score.playerScore}
+        computerScore={score.computerScore}
+        currentRound={currentRound}
+        rounds={settings.rounds}
+      />
       {choices.map((choice) => (
         <Button
           onClick={() => handleClick(choice)}
