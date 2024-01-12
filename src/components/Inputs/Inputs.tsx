843834d8d4ee1cba65e32164ceb42e32dc93f70d
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-12 17:50:29 +0100

    add input and label components

---
+++ src/components/Inputs/Inputs.tsx
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const Input = styled.input`
+  padding: 1rem 1rem;
+  border: 2px transparent;
+  border-radius: 12px;
+  outline: none;
+
+  &:hover {
+    border: 2px tomato;
+  }
+`;
