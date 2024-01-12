180b45f8c7c5e6b30e3451da5dd208a7542a5ce3
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-12 14:49:15 +0100

    add main container

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  height: 100vh;
+  gap: 1rem;
+  padding: 2rem;
+  box-sizing: border-box;
+  overflow: hidden;
+  background-color: #222;
+`;
