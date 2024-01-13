a3c9033d2be8a36407b911e8d6eeddc23d20de77
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-13 20:48:32 +0100

    update input radius

--- src/components/Inputs/Inputs.tsx
+++ src/components/Inputs/Inputs.tsx
@@ -3,7 +3,7 @@ import { styled } from "styled-components";
 export const Input = styled.input`
   padding: 1rem 1rem;
   border: 2px transparent;
-  border-radius: 12px;
+  border-radius: 1rem;
   outline: none;
   background-color: var(--bg2);
   color: var(--gray-900);
