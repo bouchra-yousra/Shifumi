3c0f8032acbe0f83af3ff83378a1f4198945c767
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-13 22:59:37 +0100

    displaying the app's background

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,13 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { PageContainer } from "../components";
-
 import { Settings } from "../components/settings/Settings";
+import { Background } from "../components/container/Background";
 
 export const Welcome = () => {
   const navigate = useNavigate();
 
   return (
     <PageContainer>
+      <Background />
       <Settings
         title={"Shifumi challenge"}
         open={true}
