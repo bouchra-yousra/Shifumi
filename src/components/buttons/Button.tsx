4afd19ebfd4f1ba8e4b34e14f16a62b91612d47e
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-13 16:13:13 +0100

    update button style

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,12 @@
 import { styled } from "styled-components";
 
 export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.4em;
   border: 2px solid transparent;
   outline: none;
+  background-color: var(--primary);
+  color: var(--primary-dark);
 `;
