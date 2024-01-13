a95ddf803e24180ba1eced2f077240ffd3158796
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-13 13:02:00 +0100

    update settings and add input wrapper

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,43 +1,98 @@
+import { useEffect, useState } from "react";
 import { GameSettings } from "../../types";
 import { Button } from "..";
 import { SettingsContainer } from "./SettingsContainer";
-import { useState } from "react";
 import { Input } from "../Inputs/Inputs";
 import { Label } from "../Inputs/Label";
+import { useAnimate, useAnimation } from "framer-motion";
+import { InputWrapper } from "../Inputs/InputWrapper";
 
 type SettingsProps = {
   updateGameSettings: (game: GameSettings) => void;
   currentSettings: GameSettings;
+  open?: boolean;
 };
 
 export const Settings = ({
   currentSettings,
   updateGameSettings,
+  open,
 }: SettingsProps) => {
   const [newSettings, setNewSettings] = useState(currentSettings);
-  return (
-    <SettingsContainer>
-      <Label>Player name</Label>
-      <Input
-        placeholder={currentSettings.playerNamer}
-        onChange={(e) =>
-          setNewSettings({ ...currentSettings, playerNamer: e.target.value })
-        }
-      />
-      <Label>Rounds number</Label>
-      <Input
-        type="number"
-        placeholder={String(currentSettings.rounds)}
-        onChange={(e) =>
-          setNewSettings({
-            ...currentSettings,
-            rounds: Number.parseInt(e.target.value),
-          })
-        }
-        min={1}
-        max={20}
-      />
+  const animationControl = useAnimation();
 
+  useEffect(() => {
+    if (open) {
+      animationControl
+        .start(
+          {
+            display: "flex",
+            opacity: 0.5,
+            translateY: 0,
+          },
+          { duration: 0.5 }
+        )
+        .then(() =>
+          animationControl.start(
+            {
+              display: "flex",
+              opacity: 1,
+            },
+            { duration: 0.5 }
+          )
+        );
+    } else {
+      animationControl
+        .start(
+          {
+            display: "flex",
+            opacity: 1,
+            translateY: 1000,
+          },
+          { duration: 0.5 }
+        )
+        .then(() =>
+          animationControl.start({
+            display: "none",
+            opacity: 0,
+          })
+        );
+    }
+  }, [open]);
+
+  return (
+    <SettingsContainer
+      initial={{
+        display: "none",
+        opacity: 0,
+        translateY: -100,
+      }}
+      animate={animationControl}
+    >
+      <InputWrapper>
+        <Label>Player name {String(open)}</Label>
+        <Input
+          placeholder={currentSettings.playerNamer}
+          onChange={(e) =>
+            setNewSettings({ ...currentSettings, playerNamer: e.target.value })
+          }
+        />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>Rounds number</Label>
+        <Input
+          type="number"
+          placeholder={String(currentSettings.rounds)}
+          onChange={(e) =>
+            setNewSettings({
+              ...currentSettings,
+              rounds: Number.parseInt(e.target.value),
+            })
+          }
+          min={1}
+          max={20}
+        />
+      </InputWrapper>
       <Button onClick={() => updateGameSettings(newSettings)}>Submit</Button>
     </SettingsContainer>
   );
--- src/components/settings/SettingsContainer.tsx
+++ src/components/settings/SettingsContainer.tsx
@@ -2,12 +2,14 @@ import { motion } from "framer-motion";
 import { styled } from "styled-components";
 
 export const SettingsContainer = styled(motion.div)`
+  position: absolute;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: stretch;
   padding: 2rem;
-
+  border-radius: 1rem;
+  gap: 1em;
   background-color: white;
   text-align: start;
 `;
