176ef2ec01b8b70fdce4f80f4b4856f3566c35b3
bouchra-yousra <bouchraafoun@gmail.com>
2024-01-13 16:14:08 +0100

    add option container and update round container in board

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -4,11 +4,15 @@ import { ScoreContainer, ScoreCard } from "./ScoreCard";
 
 import player from "../../../public/images/gamer.png";
 import robot from "../../../public/images/robot.png";
+import { OptionContainer } from "./OptionContainer";
+import { Button } from "..";
 type BoardProps = {
   playerScore: number;
   computerScore: number;
   currentRound?: number;
   rounds?: number;
+  onRestart: () => void;
+  onOpenOptions: () => void;
 };
 
 export const Board = ({
@@ -16,22 +20,38 @@ export const Board = ({
   computerScore = 0,
   rounds,
   currentRound,
+  onRestart,
+  onOpenOptions,
 }: BoardProps) => {
   return (
     <BoardContainer>
+      <RoundContainer>
+        <span>Rounds</span>
+        <span>
+          {currentRound} / {rounds}
+        </span>
+      </RoundContainer>
       <ScoreContainer>
         <ScoreCard isPlayer>
           <img src={player} width={50} />
           <span>{playerScore}</span>
         </ScoreCard>
+
         <ScoreCard>
           <span>{computerScore}</span>
           <img src={robot} width={50} />
         </ScoreCard>
       </ScoreContainer>
-      <RoundContainer>
-        Round {currentRound} of {rounds}
-      </RoundContainer>
+      <OptionContainer>
+        <Button onClick={onOpenOptions}>
+          {/* <span>Options</span> */}
+          <img src="images/settings.png" width={20} />
+        </Button>
+        <Button onClick={onRestart}>
+          {/* <span>Restart</span> */}
+          <img src="images/restart.png" width={20} />
+        </Button>
+      </OptionContainer>
     </BoardContainer>
   );
 };
--- src/components/board/OptionContainer.tsx
+++ src/components/board/OptionContainer.tsx
@@ -1,16 +1,10 @@
 import { styled } from "styled-components";
 
-export const RoundContainer = styled.div`
+export const OptionContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem 1.25rem;
-
-  width: 125px;
-  height: max-content;
   font-size: larger;
-  background-color: var(--orange-100);
   color: var(--orange-200);
-  border-radius: 1em;
 `;
